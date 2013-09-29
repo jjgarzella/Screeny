2d7656d1d8d13fa6315568a8bff644d47a41a161
jjgarzella <jjgarzella@gmail.com>
2013-09-28 19:19:29 -0600

    The app now places an image on the screen

--- Screeny/Screeny/SViewController.m
+++ Screeny/Screeny/SViewController.m
@@ -27,17 +27,32 @@
 }
 
 - (IBAction)updateScreenshot:(UITextField *)sender {
+    
     NSDictionary* headers = [NSDictionary dictionaryWithObjectsAndKeys:@"p9qYKW3aCdguDb0wICdEfLEfEwwd8LrA", @"X-Mashape-Authorization", nil];
-//    NSDictionary* parameters = [NSDictionary dictionaryWithObjectsAndKeys:nil];
+    NSNumber *heightOfImageView = [NSNumber numberWithFloat:self.webScreenshotImageView.bounds.size.height];
+    NSNumber *widthOfImageView = [NSNumber numberWithFloat:self.webScreenshotImageView.bounds.size.width];
+    
+    NSString *startingQueryURL = @"https://shepik-web-screenshot.p.mashape.com/screenshot.php?url=http%3A%2F%2Fgoogle.com%2F%3Fhl%3Den&h=";
+    NSString *queryURL = [NSString stringWithFormat:@"%@%@&w=%@", startingQueryURL, heightOfImageView, widthOfImageView];
     
     HttpJsonResponse* response = [[Unirest get:^(SimpleRequest* request) {
-        [request setUrl:@"https://shepik-web-screenshot.p.mashape.com/screenshot.php?url=http%3A%2F%2Fgoogle.com%2F%3Fhl%3Den&h=1000&w=1024"];
+        [request setUrl:queryURL];
         [request setHeaders:headers];
-//        [request setParameters:parameters];
     }] asJson];
     
     if (![response.body isArray]) {
-        NSLog(@"%@", response.body.JSONObject);
+        
+        NSDictionary *responseDictionary = response.body.JSONObject;
+        NSLog(@"%@", responseDictionary);
+        
+        NSURL *screenshotURL = [NSURL URLWithString:responseDictionary[@"image"]];
+        
+        NSData *screenshotData = [NSData dataWithContentsOfURL:screenshotURL];
+        
+        UIImage *screenshot = [UIImage imageWithData:screenshotData];
+        
+        self.webScreenshotImageView.image = screenshot;
+        
     }
 }
 @end
